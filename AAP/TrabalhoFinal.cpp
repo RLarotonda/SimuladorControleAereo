bfc44a3133ccaaf191389cfafdb2e896195e65e0
Rafael Larotonda <trizlarotonda@gmail.com>
2025-05-26 15:17:45 -0300

    TrabalhoFinal.cpp

--- AAP/TrabalhoFinal.cpp
+++ AAP/TrabalhoFinal.cpp
@@ -2,16 +2,30 @@
 #include <ctime>
 using namespace std;
 
-#include "FilaPrioridadeAdap.h"
+#include "FilaPrioridadeAdaptada.h"
+
+void tempoEspera(no *L, int espera) {
+    no* aux = L;
+    int posicao = 1;
+    cout << endl;
+    while (aux != NULL) {
+        int tempoEspera = posicao * espera;
+        cout << "Voo " << aux->info << " com espera aproximada de " << tempoEspera << " ut" << endl;
+        aux = aux->link;
+        posicao++;
+    }
+}
 
 int main() {
     // Criacao das filas de prioridade
     no* fp1 = inicializaFP(fp1);    
     no* fp2 = inicializaFP(fp2);    
 
-    no voo[50]; 
-    int idVoo = 0; // info 
+    no voo[50]; // vetor estatico
+    int idVoo = 0; // int info
     bool continua = true;
+    int esperaPouso = 2;
+    int esperaDecolagem = 3;
 
     // Cores
     const string RESET = "\033[0m"; 
@@ -22,58 +36,70 @@ int main() {
     const string MAGENTA = "\033[35m";
     const string CIANO = "\033[36m";
 
-    srand(time(0));   
+    srand(time(0));
 
     for(int ut = 0; continua; ut++) {
-        cout << VERMELHO << "\nUnidade de Tempo " << ut << RESET << endl; 
-        if(idVoo <50){
-            cout << MAGENTA << "\nChegada de 5 solicitacoes!" << RESET << endl;
+        cout << endl;
+        cout << VERMELHO << "Unidade de Tempo " << ut << RESET << endl; 
+        if(idVoo < 50){ // Pelo menos 50 voos
+            cout << MAGENTA << "Chegada de 5 solicitacoes!" << RESET << endl;
 
             for (int i = 0; i < 5; i++) {
                 voo[idVoo].info = idVoo;
-                voo[idVoo].prior = (rand()%2) + 1;     // 1 = alta, 2 = media
-                voo[idVoo].tipo = rand()%2;      
+                voo[idVoo].prior = (rand()%2) + 1; // 1 = alta, 2 = media
+                voo[idVoo].tipo = rand()%2; // 0 = pouso, 1 = decolagem
 
                 if(voo[idVoo].tipo == 0) {
                     fp1 = insereFP(fp1, idVoo, voo[idVoo].prior);
-                    cout << "\nVoo " << voo[idVoo].info << "(" << voo[idVoo].prior << ") entrou na fila de POUSO" << endl;
+                    cout << "Voo " << voo[idVoo].info << "(" << voo[idVoo].prior << ") entrou na fila de POUSO" << endl;
+                    cout << "POUSO: ";
                     exibe(fp1);
+                    tempoEspera(fp1, esperaPouso);
                     cout << endl;
                 } else {
                     fp2 = insereFP(fp2, idVoo, voo[idVoo].prior);
-                    cout << "\nVoo " << voo[idVoo].info << " (" << voo[idVoo].prior << ") entrou na fila de DECOLAGEM" << endl;
+                    cout << "Voo " << voo[idVoo].info << " (" << voo[idVoo].prior << ") entrou na fila de DECOLAGEM" << endl;
+                    cout << "DECOLAGEM: ";
                     exibe(fp2);
+                    tempoEspera(fp2, esperaDecolagem);
                     cout << endl;
                 }
                 idVoo++;
             }
         }else {
-            cout << "\nNao chegam mais solicitacoes." << endl;
+            cout << MAGENTA << "Nao chegaram mais solicitacoes." << RESET << endl;
         }
 
-        if (ut % 2 == 0 && fp1 != NULL) {
+        if (ut % 2 == 0 && fp1 != NULL) { // A cada 2 ut
+            cout << "Pista de POUSO liberada" << endl;
             int vooRemovido = 0, prioridadeRemovida = 0;
-            fp1 = removeFP(fp1, &vooRemovido, &prioridadeRemovida);
-            cout << AMARELO << "\nVoo " << vooRemovido << " (" << prioridadeRemovida << ") realizou POUSO" << RESET << endl;
+            fp1 = removeFP(fp1, &vooRemovido, &prioridadeRemovida); // Remove primeiro da fila
+            cout << AMARELO << "Voo " << vooRemovido << " (" << prioridadeRemovida << ") esta realizando POUSO" << RESET << endl;
         }
-
         
-
-        if (ut % 3 == 0 && fp2 != NULL) {
+        if (ut % 3 == 0 && fp2 != NULL) { // A cada 3 ut
+            cout << "Pista de DECOLAGEM liberada" << endl;
             int vooRemovido = 0, prioridadeRemovida = 0;
             fp2 = removeFP(fp2, &vooRemovido, &prioridadeRemovida);
-            cout << AZUL << "\nVoo " << vooRemovido << " (" << prioridadeRemovida << ") realizou DECOLAGEM" << RESET << endl;
+            cout << AZUL << "Voo " << vooRemovido << " (" << prioridadeRemovida << ") esta realizando DECOLAGEM" << RESET << endl;
         }
-        cout << VERDE << "\n=== Status das filas ===" << RESET << endl;
-        cout << VERDE << "\nPista de pouso liberada";
+
+        cout << VERDE << "=== Status das filas ===" << RESET << endl;
+        cout << CIANO << "Fila de POUSO:" << RESET;
+        cout << VERDE;
         exibe(fp1);
-        cout << RESET << endl;
+        cout << RESET;
+        tempoEspera(fp1, esperaPouso);
+        cout << endl;
 
-        cout << VERDE <<"\nPista de decolagem liberada";
+        cout << CIANO <<"Fila de DECOLAGEM:" << RESET;
+        cout << VERDE;
         exibe(fp2);
-        cout << RESET << endl;
+        cout << RESET;
+        tempoEspera(fp2, esperaDecolagem);
+        cout << endl;
 
-         if (idVoo >= 50 && fp1 == NULL && fp2 == NULL) {
+        if (idVoo >= 50 && fp1 == NULL && fp2 == NULL) { // Quando as filas estiverem vazias
             cout << "=== Fim da simulacao: Todos os voos foram processados ===";
             continua = false;
         }
